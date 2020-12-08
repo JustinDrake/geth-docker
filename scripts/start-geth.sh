408f0dc0026e44210ae01226f503762b933d956c
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2020-12-08 03:10:51 +0200

    Add max peers in .env

--- scripts/start-geth.sh
+++ scripts/start-geth.sh
@@ -5,6 +5,7 @@
 exec geth \
     --networkid $ETH_NETWORK_ID \
     --port $P2P_PORT \
+    --maxpeers $MAX_PEERS \
     --http --http.port $HTTP_PORT --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net" \
     --ipcdisable \
     --nousb

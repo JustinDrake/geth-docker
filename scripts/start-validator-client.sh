1ca42df33d6ed3ee2a0e48f8e027c7f7bd481dea
Taneli Hukkinen <3275109+hukkin@users.noreply.github.com>
2022-08-25 21:25:11 +0300

    Set suggested-fee-recipient in the validator

--- scripts/start-validator-client.sh
+++ scripts/start-validator-client.sh
@@ -14,5 +14,6 @@ fi
 exec lighthouse \
     --network $NETWORK \
     validator \
+    --suggested-fee-recipient $DEFAULT_FEE_RECIPIENT \
     --beacon-nodes http://lighthouse:5052 \
     --http --http-address=0.0.0.0 --http-port=5062 --unencrypted-http-transport

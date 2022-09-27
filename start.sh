1b3f0760b4a1043c75260f8d5d03357af1d062dc
Taneli Hukkinen <3275109+hukkin@users.noreply.github.com>
2022-09-27 16:41:07 +0300

    MEV-boost

--- start.sh
+++ start.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
-docker-compose pull &&
-docker-compose down &&
+set -e  # Exit immediately if a command exits with a non-zero status.
+set -a  # Mark variables which are modified or created for export.
+
+# Set (and export) env variables
+source .env
+
+# Make sure data dirs are created without root being the owner
+mkdir -p ${EXECUTION_DATA}
+mkdir -p ${CONSENSUS_DATA}
+mkdir -p ${VALIDATOR_DATA}
+
+if [ "$ENABLE_MEVBOOST" != "" ]; then
+    # docker-compose decides which profiles to use based on this variable
+    COMPOSE_PROFILES="mevboost"
+    # These are CLI args for Nimbus
+    LIGHTHOUSE_BN_MEVBOOST_FLAGS="--builder=http://mevboost:18550"
+    LIGHTHOUSE_VC_MEVBOOST_FLAGS="--builder-proposals"
+fi
+
+docker-compose pull
+docker-compose down
 docker-compose up -d
